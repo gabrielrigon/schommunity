0d6483a2f145be72cff2663df4765411bf715c1d
Gabriel Rigon <gabrielrigon@outlook.com>
2017-05-28 03:15:56 -0300

    pequenos ajustes

--- app/controllers/teachers/dashboard_controller.rb
+++ app/controllers/teachers/dashboard_controller.rb
@@ -16,15 +16,21 @@ class Teachers::DashboardController < InheritedResources::Base
 
   def index
     if current_user.teacher?
-      classrooms_ids = Classroom.where("teacher_id = #{current_user.id} or helper_id = #{current_user.id}").ids
-      classrooms_ids = classrooms_ids.flatten.uniq.reject { |item| item.nil? || item == '' }
+      classrooms_ids = Classroom.where("teacher_id = #{current_user.id} or helper_id = #{current_user.id}")
+                                .ids
+      classrooms_ids = classrooms_ids.flatten.uniq
+                                     .reject { |item| item.nil? || item == '' }
 
       @classrooms = Classroom.where(id: classrooms_ids)
       @posts = Post.valid.where { classroom_id.in classrooms_ids }
+                   .order('created_at desc').last(15)
     elsif current_user.schoolmaster?
-      @registered_users = User.where(institution: current_user.institution).count
-      @classrooms_count = Classroom.where(institution: current_user.institution).count
-      @posts = Post.valid.where(institution: current_user.institution).order('created_at desc').last(15)
+      @registered_users = User.where(institution: current_user.institution)
+                              .count
+      @classrooms_count = Classroom.where(institution: current_user.institution)
+                                   .count
+      @posts = Post.valid.where(institution: current_user.institution)
+                   .order('created_at desc').last(15)
     end
   end
 end
